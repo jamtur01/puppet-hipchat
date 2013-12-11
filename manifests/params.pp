d7b7bd3510a30524e42b3bd224b4e41b2b124320
Zach Leslie <zach@puppetlabs.com>
2013-12-11 11:45:54 -0800

    (maint) Add support for the PE file owner
    This adds the ability to set the owner and group of the hipchat.yaml
    file based on which Puppet is being run.

--- manifests/params.pp
+++ manifests/params.pp
@@ -5,8 +5,12 @@ class puppet_hipchat::params {
   if $::is_pe == 'true' {
     $puppetconf_path = '/etc/puppetlabs/puppet'
     $provider        = 'pe_gem'
+    $owner           = 'pe-puppet'
+    $group           = 'pe-puppet'
   } else {
     $puppetconf_path = '/etc/puppet'
     $provider        = 'gem'
+    $owner           = 'puppet'
+    $group           = 'puppet'
   }
 }
