5e8f6c002e3da089a9763b1f1da3ecb5797e919e
James Powis <james.powis@viawest.com>
2016-03-09 13:46:08 -0700

    General code quality improvements
    Changes made
    1: force stringified booleans to be real booleans
    
    2: hipchat gem fails back to using environmental variable for proxy config,
    given the error logging we need to fall back as well
    
    3: hipchat pivots proxy on if defined, passing nil is an accpetable, as such no
    if statement is required so long as real boolean is passed (requires #1)
    
    4: added management of proxy settings into module
    
    5: added spec tests for puppetboard and dashboard settings in config
    file
    
    6: rather than setting strings to false, they should be not written to
    the config file at all. Implemented
    
    7: since #1 destringed booleans, removed checks for 'false'
    
    8: none of the valid values in the yaml config file would receive
    special meaning, removing quotes

--- lib/puppet/reports/hipchat.rb
+++ lib/puppet/reports/hipchat.rb
@@ -13,6 +13,17 @@ Puppet::Reports.register_report(:hipchat) do
   raise(Puppet::ParseError, "Hipchat report config file #{configfile} not readable") unless File.exist?(configfile)
   config = YAML.load_file(configfile)
 
+  #due to previous code stringifying false in place of undef we need to hard set those values to undef if 'false' for backward compatibility
+  if config[:hipchat_dashboard] == 'false'
+    config.delete(':hipchat_dashboard')
+  end
+  if config[:hipchat_dashboard] == 'false'
+    config.delete(':hipchat_dashboard')
+  end
+  if config[:hipchat_proxy] == 'false'
+    config.delete(':hipchat_proxy')
+  end
+
   HIPCHAT_API = config[:hipchat_api]
   HIPCHAT_SERVER = config[:hipchat_server]
   HIPCHAT_ROOM = config[:hipchat_room]
@@ -28,7 +39,7 @@ Puppet::Reports.register_report(:hipchat) do
   HIPCHAT_UNCHANGED_COLOR = config[:unchanged_color] || 'gray'
 
   DISABLED_FILE = File.join([File.dirname(Puppet.settings[:config]), 'hipchat_disabled'])
-  HIPCHAT_PROXY = config[:hipchat_proxy]
+  HIPCHAT_PROXY = config[:hipchat_proxy] || ENV['http_proxy']
 
   if HIPCHAT_PROXY && (RUBY_VERSION < '1.9.3' || Gem.loaded_specs["hipchat"].version < Gem::Version.new('1.0.0'))
     raise(Puppet::SettingsError, "hipchat_proxy requires ruby >= 1.9.3 and hipchat gem >= 1.0.0")
@@ -69,16 +80,13 @@ Puppet::Reports.register_report(:hipchat) do
     if (HIPCHAT_STATUSES.include?(self.status) || HIPCHAT_STATUSES.include?('all')) && !disabled
       Puppet.debug "Sending status for #{self.host} to Hipchat channel #{HIPCHAT_ROOM}"
         msg = "Puppet run for #{self.host} #{emote(self.status)} #{self.status} at #{Time.now.asctime} on #{self.configuration_version} in #{self.environment}"
-        if HIPCHAT_PUPPETBOARD != 'false'
+        if HIPCHAT_PUPPETBOARD
           msg << ": #{HIPCHAT_PUPPETBOARD}/report/latest/#{self.host}"
-        elsif HIPCHAT_DASHBOARD != 'false'
+        elsif HIPCHAT_DASHBOARD
           msg << ": #{HIPCHAT_DASHBOARD}/nodes/#{self.host}/view"
         end
-        if HIPCHAT_PROXY
-          client = HipChat::Client.new(HIPCHAT_API, :http_proxy => HIPCHAT_PROXY, :api_version => HIPCHAT_API_VERSION, :server_url => HIPCHAT_SERVER)
-        else
-          client = HipChat::Client.new(HIPCHAT_API, :api_version => HIPCHAT_API_VERSION, :server_url => HIPCHAT_SERVER)
-        end
+        
+        client = HipChat::Client.new(HIPCHAT_API, :http_proxy => HIPCHAT_PROXY, :api_version => HIPCHAT_API_VERSION, :server_url => HIPCHAT_SERVER)
         client[HIPCHAT_ROOM].send('Puppet', msg, :notify => HIPCHAT_NOTIFY, :color => color(self.status), :message_format => 'text')
     end
   end
